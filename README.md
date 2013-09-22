83e437219ae2c5aabadc8b896b6d6f8f540e2f42
Omkiran Sharma <mail.av501@gmail.com>
2013-09-22 18:14:55 +0800

    Code refactoring and Readme update

--- README.md
+++ README.md
@@ -1,12 +1,26 @@
 Intelligent Thumbnailer
 =======================
 
-Select good thumbnails from the video.
+Select set of good thumbnails for a video.
 
-Currently supports only entropy ad brightness check. i.e. Chooses thumbnails that have high entropy and reasonable brightness. See notes below for details
+A set of good thumbnails can be described as a collection that
+
+* Is evenly spread across the video to be representative about the video
+* Are different from each other as much as possible.
+* Each thumbnail is a good i.e. satisfies the following criteria
+  * Not too dark and not too bright
+  * Sharp (without motion blur)
+  * Has some content (not plain sky or blank wall) [Hence some entropy]
+  * Ideally has a face or prefferably more than 1 but should not be closeup of face.
+
+
+Current Status
+--------------
+
+Supports only entropy and brightness check. i.e. Chooses thumbnails that have high entropy and reasonable brightness. See notes below for details
 
 Dependencies
------------
+------------
 
 Needs ffmpeg already installed on system
 Needs ruby-ffmpeg and trollop gems
@@ -48,3 +62,16 @@ Blur: Frames that have blur in them are not good canidates. They may have high e
 Face: It is always perferrable to get faces if possible. Our algorithm prefers 2-3 faces over 1 face. But all faces together must not occupy more than 30% of screen space, else its probably a face blow up. 
 
 Correlation: Ideally the thumbnails should not be very similar to each other. This is possible if scenes repeat with very high correlation between them. 
+
+
+Algorithm
+---------
+
+Logically divides the video into 2N chunks where N is the number of thumbnails requested. 
+Currently simply randomly chooses searchsteps number of frames within in chunk and calculates the brightness and entropy of each frame.
+Selects frames with highest entropy and brightness in each chunk
+Selects the top N frames from each chunk.
+
+
+Changes to algorithm coming soon
+Add blur detection and correlation metric. Correlation metric will try to minimize correlation between chosen frames
--- itg.rb
+++ itg.rb
@@ -2,41 +2,44 @@ require 'trollop'
 require 'ruby-ffmpeg'
 require_relative './y4mreader'
 
+
+# Stubs
 def calc_blur( imageY )
     return 0;
 end
+
 def face_metric( thumb )
     return 1;
 end
 
-def pixel_metrics( t )
+# 
+def pixel_metrics thumbnail
     entropy    = 0.0
     brightness = 0.0
-    y4m = Y4Mreader.new t[:file]
+    y4m = Y4Mreader.new thumbnail[:file]
     y4m.read_header
     size = y4m.w * y4m.h
     imageY = y4m.read(size).each_char.to_a
+    # Get brighness between 0 to 1
     brightness = ( imageY.map{ |x| x.ord }.reduce(:+)  / size.to_f ) / 255.0
     if brightness > 0.75 # too bright is also bad.. so we are pushing the extreme bright to low scores
+        brightness = 1.0 - brightness;
     elsif brightness > 0.25 || brightness < 0.75
         brightness = 2*brightness; # Middle range is pushed to higher scores
     end
-    image_hist     = imageY.group_by{ |x| x.ord }
-    image_hist.each do | k, v | image_hist[k] =  v.size end
+    # Get histogram of image and calulate entropy as sum(for all p)[p*log2(p)] * -1
+    image_hist = imageY.group_by{ |x| x.ord }
+    image_hist.each{ | k, v | image_hist[k] = v.size}
     entropy = image_hist.values.collect{ |x| (x/size.to_f) * Math.log(x/size.to_f) }.reduce(:+) * -1.0/8.0
-    [brightness, entropy]
+    # Divide by 8.0 because we have 256 bins and we are not log to the base 2 but to base 10.
+    {:brightness => brightness, :entropy => entropy}
 end
 
 
-def get_metrics( thumbs, opts , ts, i)
-    metrics = []
-    thumbs.each_with_index do | t, j |
-        brightness, entropy = pixel_metrics( t )
-        blur = 0
-        face = face_metric( t )
-        metrics << { :brightness => brightness, :blur => blur, :entropy => entropy, :face => face , :ts => ts[i][j], :chunk => i}
-    end
-    metrics
+def get_metrics( thumbs, ts, i, opts)
+    thumbs.each_with_index.collect{ |t, j|
+            pixel_metrics( t ).merge( { :face => 0, :blur => 0, :ts => ts[j], :chunk => i} )
+    }
 end
 
 def optimize(metrics, opts)
@@ -50,52 +53,50 @@ def optimize(metrics, opts)
         best[m.max] = metrics[i][m.rindex(m.max)][:ts]
     end
     values = []
-    puts best.size/2 
-    # Now optimize for distance, correlation and face
+    # Now optimize for distance, correlation and face simultaneously
+    # 
     best.sort[0..best.size/2].map{ |k,v| values << v }
     values
 end
 
 def intelligent_thumbs(movie, opts)
     # Divide the movie into L = 2*N parts
-    l = 2*opts[:num]
     ts     = []
     thumbs = []
     metrics = []
-    # Select x random snapshots in each part 
-    chunk_length = movie.duration / l
-    (0...l).each do | i |
-        ts[i] ||= []
-        thumbs[i] ||= []
-        thumbnails ||= []
-        opts[:search_steps].times{ |j| ts[i] << rand(i*chunk_length..(i+1)*(chunk_length)) }
-    end
-    ts.size.times{ |i| thumbs[i] = movie.thumbnails(ts[i]) }
-    # Now do a metrics check on each of the snapshots and gather them
-    thumbs.size.times{ |i| metrics[i] = get_metrics( thumbs[i], opts, ts, i ) }
+    # We start with 2x the number of chunks as required
+    chunk_length = movie.duration / (2 * opts[:num] )
+    # Select random timestamps for each chunk
+    ts = Array.new(2*opts[:num]){ |i| Array.new(opts[:search_steps]){ |j| rand(i*chunk_length..(i+1)*chunk_length) } }
+    # Now get thumbnails for each chunk
+    thumbs = Array.new(ts.size){ |i| movie.thumbnails(ts[i]) }
+    # Now get the metrics
+    metrics = Array.new(ts.size){ |i| get_metrics( thumbs[i], ts[i], i, opts) }
     # Once metrics are available choose the best 2 from each chunk and then optimize
     final_set = optimize(metrics, opts)
 end
 
+def dumb_thumbs(movie, opts)
+    chunk_length = movie.duration / opts[:num]
+    (0...l).collect{ |i| rand(i*chunk_length..(i+1)*chunk_length) }
+end
+
+
+# Handle the input parameters
 opts = Trollop::options do
     banner "Intelligent thumbnails. Omkiran Sharma (c) 2013"
-    opt :input,      "Input file", :required => true,  :type => :string
-    opt :num,        "Number of thumbnails",           :type => :integer, :default => 5
-    opt :act_dumb,   "Do not use intelligence,random", :type => :boolean, :default => false
-    opt :prefix,     "Output name prefix",             :type => :boolean
-    opt :format,     "Format of output thumbnail",     :type => :string,  :default => "jpeg"
-    opt :resolution, "Resolution of output",           :type => :string,  :default => "copy"
-    opt :search_steps,"Search steps",                  :type => :integer, :default => 3
+    opt :input,       "Input file", :required => true,            :type => :string
+    opt :num,         "Number of thumbnails",                     :type => :integer, :default => 5
+    opt :act_dumb,    "Do not use intelligence,random selection", :type => :boolean, :default => false
+    opt :prefix,      "Output name prefix",                       :type => :boolean
+    opt :format,      "Format of output thumbnail",               :type => :string,  :default => "jpeg"
+    opt :resolution,  "Resolution of output",                     :type => :string,  :default => "320x240"
+    opt :search_steps,"Number of intermediate files",             :type => :integer, :default => 3
 end
 
 # For the input given, if we are to act dumb, sample randomly and be done
-movie = FFMPEG::Movie.new( opts[:input] )
+movie    = FFMPEG::Movie.new( opts[:input] )
 duration = movie.duration
-final_set = []
-if opts[:act_dumb]
-    opts[:num].times do final_set << rand(0..duration) end
-else
-    final_set = intelligent_thumbs(movie, opts)
-end
+final_set = opts[:act_dumb] ? dumb_thumbs( movie, opts ) : intelligent_thumbs( movie, opts )
 system("rm *.y4m")
-thumbs = movie.thumbnails(final_set)
+thumbs = movie.thumbnails(final_set, { :format => opts[:format], :prefix => opts[:prefix], :resolution => opts[:resolution] } )
